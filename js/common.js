db2bc2088ce483098c099157fb97af915d811bd7
K-ost <golubkovru@gmail.com>
2024-10-08 17:48:01 +0300

    faq, js, mobile

--- js/common.js
+++ js/common.js
@@ -17,3 +17,46 @@ if (document.documentElement.clientWidth < 1200) {
     });
   });
 }
+
+// spoiler
+function spoilersFunction() {
+  const spoilerTitles = document.querySelectorAll(".js-spoiler");
+  spoilerTitles.forEach((item) => {
+    let showTitle = "";
+
+    if (
+      document.documentElement.clientWidth >= 1200 &&
+      item.classList.contains("footer_item-title")
+    ) {
+      return;
+    }
+
+    let hideTitle = item.getAttribute("data-title");
+    const children = item.closest(".spoiler").children;
+    const body = Object.values(children).find(
+      (el) => el.className === "spoiler-body"
+    );
+
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      const bodyHeight = body.children[0].offsetHeight;
+
+      if (!item.classList.contains("active")) {
+        body.style.height = `${bodyHeight}px`;
+        item.classList.add("active");
+        if (hideTitle) {
+          showTitle = item.textContent;
+          item.textContent = hideTitle;
+        }
+      } else {
+        body.removeAttribute("style");
+        item.classList.remove("active");
+        body.style.height = "0px";
+        if (hideTitle) {
+          item.textContent = showTitle;
+        }
+      }
+    });
+  });
+}
+spoilersFunction();
